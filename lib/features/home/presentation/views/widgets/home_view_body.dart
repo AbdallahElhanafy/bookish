b99adbf089458a6a95e653550ad49bb71209ec6b
Abdallah Usama <abdallah.usama.biz@gmail.com>
2023-08-08 15:29:59 +0300

    AppBar

---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,42 @@
+import 'package:ebook_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [CustomAppBar()],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40).r,
+      child: Row(
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 18.h,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24.r,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
