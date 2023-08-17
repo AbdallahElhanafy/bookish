22b3ffbdfd5e4a60db31a96eb1f7776eee92e2ea
Abdallah Usama <abdallah.usama.biz@gmail.com>
2023-08-17 16:34:29 +0300

    service locator

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  setup();
+  setupServiceLocator();
   runApp(const BooksApp());
 }
 
